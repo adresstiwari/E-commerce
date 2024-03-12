bc03bd255fa1e68a2c3df5c3074c4a42de12e823
adresstiwari <adress2762000@gmail.com>
2024-03-12 23:48:32 +0530

    added basic routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import morgan from 'morgan';
 import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 // configure env
 dotenv.config();
@@ -14,7 +15,10 @@ const app = express ()
 // middelwares 
 app.use(express.json())
 app.use(morgan('dev'))
+//routes
+app.use("/api/v1/auth", authRoutes)
 
+//rest api
 app.get('/',(req,res)=>{
     res.send({
         message:'Welcome to E-commerce app'
