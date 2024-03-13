096104897926f752d13074997ba078d3d20466ac
adresstiwari <adress2762000@gmail.com>
2024-03-13 12:17:27 +0530

    login and jwt token created

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,13 @@
 import express from "express";
-import{registerController} from '../controllers/authControllers.js'
+import{registerController,loginController} from '../controllers/authControllers.js'
 
 // router object
 const router = express.Router()
 
 //routing
+// REGISTER METHOD POST
 router.post('/register',registerController)
 
+//LOGIN POST
+router.post('/login',loginController)
 export default router;
